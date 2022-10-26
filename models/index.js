2f904553bc7abf631cb5a6efec79a4d930fe9fa8
Will W <will.winston@gmail.com>
2022-10-25 20:48:41 -0400

    Adding seed data and utility functions third try

--- models/index.js
+++ models/index.js
@@ -1,33 +1,30 @@
+27 lines (21 sloc)  668 Bytes
+
 const User = require('./user');
-const Ciphers = require('./ciphers');
-const EncryptionKeys = require('./encryption_keys');
+const Passphrase = require('./passphrase');
+const EncryptedPwd = require('./encrypted_pwd');
 const OneTimePasscode = require('./onetime_passcode');
 const RainbowTable = require('./rainbow_table');
 const SearchContent = require('./search_content')
 
-User.hasMany(Ciphers);
-Ciphers.belongsTo(User);
-EncryptionKeys.hasOne(Ciphers);
-Ciphers.hasOne(EncryptionKeys);
+User.hasMany(Passphrase, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Passphrase.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+EncryptedPwd.hasOne(Passphrase, {
+  foreignKey: 'cipher_id',
+  onDelete: 'CASCADE'
+});
+
+Passphrase.hasOne(EncryptedPwd, {
+  foreignKey: 'cipher_id'
+});
 
 
-module.exports = { User, Ciphers, EncryptionKeys, OneTimePasscode, RainbowTable, SearchContent }
-
-// User.hasMany(Ciphers, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
-
-// Ciphers.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
-
-// EncryptionKeys.hasOne(Ciphers, {
-//   foreignKey: 'cipher_id',
-//   onDelete: 'CASCADE'
-// });
-
-// Ciphers.hasOne(EncryptionKeys, {
-//   foreignKey: 'cipher_id'
-// });
+module.exports = { User, Passphrase, EncryptedPwd, OneTimePasscode, RainbowTable, SearchContent }
 
