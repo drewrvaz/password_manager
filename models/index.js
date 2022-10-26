d30195295b799eeac15b6356e9131d3c1734f10f
Will W <will.winston@gmail.com>
2022-10-25 20:42:40 -0400

    Adding seed data and utility functions

--- models/index.js
+++ models/index.js
@@ -5,23 +5,29 @@ const OneTimePasscode = require('./onetime_passcode');
 const RainbowTable = require('./rainbow_table');
 const SearchContent = require('./search_content')
 
-User.hasMany(Ciphers, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Ciphers.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-EncryptionKeys.hasOne(Ciphers, {
-  foreignKey: 'cipher_id',
-  onDelete: 'CASCADE'
-});
-
-Ciphers.hasOne(EncryptionKeys, {
-  foreignKey: 'cipher_id'
-});
+User.hasMany(Ciphers);
+Ciphers.belongsTo(User);
+EncryptionKeys.hasOne(Ciphers);
+Ciphers.hasOne(EncryptionKeys);
 
 
 module.exports = { User, Ciphers, EncryptionKeys, OneTimePasscode, RainbowTable, SearchContent }
+
+// User.hasMany(Ciphers, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Ciphers.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+
+// EncryptionKeys.hasOne(Ciphers, {
+//   foreignKey: 'cipher_id',
+//   onDelete: 'CASCADE'
+// });
+
+// Ciphers.hasOne(EncryptionKeys, {
+//   foreignKey: 'cipher_id'
+// });
+
